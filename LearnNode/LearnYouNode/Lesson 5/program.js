e6a83547a84582d5ff3e3ef53242e679ca9a42e8
Crostantin <crostantin@gmail.com>
2016-03-04 15:55:22 +0100

    Add lesson 5

---
+++ LearnNode/LearnYouNode/Lesson 5/program.js
@@ -0,0 +1,17 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+var fs = require ("fs");
+var path = require ("path");
+
+var extension = process.argv[3];
+
+var list = fs.readdir(process.argv[2], function(err, list) {
+
+	list.forEach ( function(list){
+		if ( path.extname(list) === extension ) {
+				console.log (list);
+			}
+		});
+});
